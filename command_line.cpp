bec72ad0e4faf29d0a51d25de588eb02755d7f7f
dixitix <87375463+dixitix@users.noreply.github.com>
2023-06-09 23:40:11 +0300

    Update command_line.cpp

--- command_line.cpp
+++ command_line.cpp
@@ -1,8 +1,25 @@
 #include "command_line.h"
-#include "user_interface.h"
 #include "file_system.h"
+#include "user_interface.h"
 
 #include <string.h>
+
+void display_help() {
+    printf("Файловый менеджер\n");
+    printf("Использование:\n");
+    printf("  fm cp <source_path> <destination_path>   : Копировать файлы/папки\n");
+    printf("  fm mv <source_path> <destination_path>   : Переместить файлы/папки\n");
+    printf("  fm rm <file_path>                        : Удалить выбранные файлы/папки\n");
+    printf("  fm mkdir <directory_path>                : Создать новую папку\n");
+    printf("  fm touch <file_path>                     : Создать новый файл\n");
+    printf("  fm ls <directory_path>                   : Просмотр файловой системы\n");
+    printf("  fm cd <directory_path>                   : Переход между директориями\n");
+    printf("  fm cat <file_path>                       : Просмотр содержимого файла\n");
+    printf("  fm info <file_path>                      : Просмотр информации о файле\n");
+    printf("  fm grep <file_name>                      : Поиск файлов\n");
+    printf("  fm ls -t <directory_path>                : Сортировка файлов по времени создания\n");
+    printf("  fm ls -S <directory_path>                : Сортировка файлов по размеру\n");
+}
 
 int parse_arguments(int argc, char *argv[]) {
     // Проверяем, есть ли какие-либо аргументы
@@ -12,7 +29,9 @@ int parse_arguments(int argc, char *argv[]) {
     }
 
     // Проверяем, какой аргумент передан и выполняем соответствующее действие
-    if (strcmp(argv[1], "ls") == 0) {
+    if (strcmp(argv[1], "help") == 0) {
+        display_help();
+    } else if (strcmp(argv[1], "ls") == 0) {
         if (argc >= 3) {
             display_list(argv[2]);
         } else {
@@ -36,25 +55,31 @@ int parse_arguments(int argc, char *argv[]) {
         }
     } else if (strcmp(argv[1], "info") == 0) {
         if (argc >= 3) {
-            display_file_info(argv[2]);
+            if (is_path_a_directory(argv[1])) {
+                display_directory_info(argv[1]);
+            } else {
+                display_file_info(argv[1]);
+            }
         } else {
             display_error_message("Файл не указан.");
         }
     } else if (strcmp(argv[1], "cp") == 0) {
         if (argc >= 4) {
-            copy_file(argv[2], argv[3]);
+            my_copy(argv[2], argv[3]);
         } else {
-            display_error_message("Неправильное использование команды cp. Используйте cp source_file destination_file");
+            display_error_message("Неправильное использование команды cp. "
+                                  "Используйте cp source_file destination_file");
         }
     } else if (strcmp(argv[1], "mv") == 0) {
         if (argc >= 4) {
             move_file(argv[2], argv[3]);
         } else {
-            display_error_message("Неправильное использование команды mv. Используйте mv source_file destination_file");
+            display_error_message("Неправильное использование команды mv. "
+                                  "Используйте mv source_file destination_file");
         }
     } else if (strcmp(argv[1], "rm") == 0) {
         if (argc >= 3) {
-            delete_file(argv[2]);
+            my_delete(argv[2]);
         } else {
             display_error_message("Файл не указан.");
         }
@@ -70,8 +95,18 @@ int parse_arguments(int argc, char *argv[]) {
         } else {
             display_error_message("Файл не указан.");
         }
+    } else if (strcmp(argv[1], "grep") == 0 && argc == 3) {
+        find_files(".", argv[2]);
+    } else if (strcmp(argv[1], "ls") == 0 && argc == 4) {
+        if (strcmp(argv[2], "-t") == 0) {
+            list_sorted_files(argv[3], true);
+        } else if (strcmp(argv[2], "-S") == 0) {
+            list_sorted_files(argv[3], false);
+        }
+    } else if (strcmp(argv[1], "cur_d") == 0) {
+        display_current_directory();
     } else {
-        display_error_message("Неизвестная команда. Доступные команды: ls, cd, cat, info, cp, mv, rm, mkdir, touch");
+        display_error_message("Неизвестная команда. Введите fm help, чтобы посмотреть список доступных команд");
     }
     return 0;
 }

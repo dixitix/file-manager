05782a611a8b6106a259beb0d88e3711b3d265ec
dixitix <87375463+dixitix@users.noreply.github.com>
2023-06-09 23:52:46 +0300

    Update security.h

--- security.h
+++ security.h
@@ -1,6 +1,6 @@
 #ifndef FILE_MANAGER_SECURITY_H
 #define FILE_MANAGER_SECURITY_H
 
-int is_file_accessible(const char* path);
+int is_accessible(const char* path);
 
 #endif //FILE_MANAGER_SECURITY_H

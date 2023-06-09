73e0ed26dd0586b515b9f30a6c63dade6ab48bef
dixitix <87375463+dixitix@users.noreply.github.com>
2023-06-09 23:52:31 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
-#include <iostream>
-
 #include "command_line.h"
+#include "testing.h"
 
 int main(int argc, char *argv[]) {
-    int result;
+    //run_tests();
+    
+    int result = 0;
     result = parse_arguments(argc, argv);
     return result;
 }
